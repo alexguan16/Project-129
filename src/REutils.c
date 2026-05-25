593546e3d9c4ad1ae439a081a3ae85e777d0dabc
alexguan16 <alexandergguan@gmail.com>
2026-05-25 19:34:41 -0400

    fixed bounds bug, optimize guard statements, use AV2 mask convention

--- src/REutils.c
+++ src/REutils.c
@@ -2,9 +2,9 @@
 #include <stddef.h>
 #include <immintrin.h>
 #include <string.h>
-#include <stdio.h>
 
-void* memSearch(uint8_t* haystack, size_t hSz, uint8_t* needle, size_t nSz) {
+
+inline void* memSearch(uint8_t* haystack, size_t hSz, uint8_t* needle, size_t nSz) {
 	size_t i;
 	for(i = 0; i <= hSz - nSz; i++) {
 		if(0 == memcmp(haystack + i, needle, nSz)) return haystack + i;
@@ -15,13 +15,15 @@ void* memSearch(uint8_t* haystack, size_t hSz, uint8_t* needle, size_t nSz) {
 
 __attribute__((force_align_arg_pointer))
 void* memSearchAVX2(uint8_t* haystack, size_t hSz, uint8_t* needle, size_t nSz) {
+	if(hSz + (uintptr_t)haystack % 32 < 64
+		|| nSz < 2) return memSearch(haystack, hSz, needle, nSz);
+
 	if((uintptr_t)haystack % 32 != 0) {
 		for(; (uintptr_t)haystack % 32 != 0; hSz--, haystack++) {
 			if(hSz < nSz) return NULL;
 			if(0 == memcmp(haystack, needle, nSz)) return haystack;
 		}
 	}
-	if(nSz < 2 || nSz >= hSz || hSz <= 32) return NULL;
 
 	__m256i b1 = _mm256_set1_epi8(needle[0]);
 	__m256i b2 = _mm256_set1_epi8(needle[1]);
@@ -29,14 +31,14 @@ void* memSearchAVX2(uint8_t* haystack, size_t hSz, uint8_t* needle, size_t nSz) 
 	uint8_t lastByte = needle[nSz -1];
 	size_t i = 0;
 
-	for(; i < hSz-32-1; i += 32) {
+	for(; i + 32+32 <= hSz; i += 32) {
 		__m256i next = _mm256_load_si256((__m256i const*)(haystack + i + 32));
 		__m256i comb = _mm256_permute2x128_si256(curr, next, 0x21);
 		__m256i shft = _mm256_alignr_epi8(comb, curr, 1);
 
 		__m256i m1 = _mm256_cmpeq_epi8(curr, b1);
 		__m256i m2 = _mm256_cmpeq_epi8(shft, b2);
-		__mmask32 mask = _mm256_movemask_epi8(_mm256_and_si256(m1, m2));
+		uint32_t mask = _mm256_movemask_epi8(_mm256_and_si256(m1, m2));
 
 		while(mask != 0) {
 			uint32_t off = _tzcnt_u32(mask);
@@ -51,7 +53,7 @@ void* memSearchAVX2(uint8_t* haystack, size_t hSz, uint8_t* needle, size_t nSz) 
 		curr = next;
 	}
 
-	for(;i <= hSz - nSz; i++) {
+	for(;i + nSz <= hSz; i++) {
 		if(0 == memcmp(haystack+i, needle, nSz)) return haystack + i;
 	}
 
